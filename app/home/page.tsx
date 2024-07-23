e547d4d31636cfdd8c39acc3424cd8e785ca0c6a
Jose Wilfredo <wilfredon163@gmail.com>
2024-07-23 07:53:12 -0600

    chore: rollback

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -6,7 +6,7 @@ import UserProfile from 'app/components/UserProfile';
 const HomePage: React.FC = () => {
   return (
     <div className="container mx-auto p-4">
-    {/* <UserProfile /> */}
+    <UserProfile />
     <h2 className="text-2xl font-bold mb-4">Home</h2>
     <UserList />
   </div>

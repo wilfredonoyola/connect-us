3813cea770cda8f137308a1c53df64831b33f5e9
Jose Wilfredo <wilfredon163@gmail.com>
2024-07-23 07:50:04 -0600

    chore: minor changes

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import UserList from '../components/UserList';
 import UserProfile from 'app/components/UserProfile';
 
@@ -6,7 +6,7 @@ import UserProfile from 'app/components/UserProfile';
 const HomePage: React.FC = () => {
   return (
     <div className="container mx-auto p-4">
-    <UserProfile />
+    {/* <UserProfile /> */}
     <h2 className="text-2xl font-bold mb-4">Home</h2>
     <UserList />
   </div>
